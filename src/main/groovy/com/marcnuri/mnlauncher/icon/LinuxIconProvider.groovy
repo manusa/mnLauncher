8c0c91bb2d4f8e7eec88e987ee25d3139d9694cd
Marc Nuri <marc@marcnuri.com>
2017-10-10 07:22:44 +0200

    Linux Icon Provider Added support for hicolor icons

--- src/main/groovy/com/marcnuri/mnlauncher/icon/LinuxIconProvider.groovy
+++ src/main/groovy/com/marcnuri/mnlauncher/icon/LinuxIconProvider.groovy
@@ -3,20 +3,29 @@ package com.marcnuri.mnlauncher.icon
 import com.marcnuri.mnlauncher.MenuEntry
 
 import javax.imageio.ImageIO
-import java.awt.Image
+import java.awt.*
 import java.nio.file.Files
 
+/**
+ * https://standards.freedesktop.org/icon-theme-spec/icon-theme-spec-latest.html
+ *
+ */
 class LinuxIconProvider implements IconProvider {
 
 	private static final String XDG_DATA_DIRS = System.getenv("XDG_DATA_DIRS")
 	private static final String XDG_DATA_DIRS_ICON_SUFFIX = "/icons"
+	private static final String XDG_DATA_DIRS_HICOLOR_SUFFIX = "/hicolor/48x48/apps"
 	private static final String PIXMAPS = "/usr/share/pixmaps"
+	private static final String SH_SCRIPT_EXTENSION = ".sh"
 
 	private final boolean isLinux
+	private final Map<String, File> hicolorIcons
 	private final Map<String, File> pixmapsIcons
+
 
 	LinuxIconProvider() {
 		isLinux = System.getProperty(OS_PROPERTY_NAME).toLowerCase().contains("linux")
+		hicolorIcons = new HashMap<>()
 		pixmapsIcons = new HashMap<>()
 		initIndexes()
 	}
@@ -25,9 +34,16 @@ class LinuxIconProvider implements IconProvider {
 	Image getIcon(MenuEntry menuEntry) {
 		Image ret = null
 		File command = new File(menuEntry.getFirstCommand())
-		if (command.exists()) {
-			if (pixmapsIcons.containsKey(command.getName())) {
-				ret = ImageIO.read(pixmapsIcons.get(command.getName()))
+		if (command.exists() && command.isFile()) {
+			for(Map<String, File> fileMap : Arrays.asList(hicolorIcons, pixmapsIcons)) {
+				final String cName = command.getName()
+				ret = locateIcon(fileMap, cName)
+				// Locate sh script icon by name
+				if (ret == null && cName.toLowerCase().endsWith(SH_SCRIPT_EXTENSION)) {
+					ret = locateIcon(fileMap,
+							cName.substring(0, cName.length() - SH_SCRIPT_EXTENSION.length()))
+				}
+				if (ret != null) break
 			}
 		}
 		return ret
@@ -42,20 +58,33 @@ class LinuxIconProvider implements IconProvider {
 		// Load XDG Icons
 		if (XDG_DATA_DIRS != null) {
 			for(String xdgDir : XDG_DATA_DIRS.split(":")) {
-				File xdgDirFile = new File(XDG_DATA_DIRS.concat(XDG_DATA_DIRS_ICON_SUFFIX))
+				File xdgDirFile = new File(xdgDir.concat(XDG_DATA_DIRS_ICON_SUFFIX))
 				if (xdgDirFile.exists() && xdgDirFile.isDirectory()) {
-
+					addIconsToMap(hicolorIcons, new File(xdgDirFile, XDG_DATA_DIRS_HICOLOR_SUFFIX))
 				}
 			}
 		}
 		// Load pixmaps Icons
-		File pixmaps = new File(PIXMAPS)
-		if (pixmaps.exists() && pixmaps.isDirectory()) {
-			Files.list(pixmaps.toPath())
-				.filter({p -> Files.isRegularFile(p)})
-				.forEach({ p -> pixmapsIcons.put(
-					p.getFileName().toString().replaceFirst("\\..*\$", ""), p.toFile())
-				})
+		addIconsToMap(pixmapsIcons, new File(PIXMAPS))
+	}
+
+	private static final Image locateIcon(Map<String, File> map, String name) {
+		Image ret = null;
+		if (map.containsKey(name)) {
+			ret = ImageIO.read(map.get(name))
+		}
+		return ret;
+	}
+
+	private static final addIconsToMap(Map<String, File> map, File iconDir) {
+		if(iconDir.exists() && iconDir.isDirectory()) {
+			Files.list(iconDir.toPath())
+				.filter({ p -> Files.isRegularFile(p) })
+				.forEach({ p ->
+					// Add file name without extension
+					map.put(p.getFileName().toString().replaceFirst(
+							"\\..*\$", ""), p.toFile())
+			})
 		}
 	}
 
