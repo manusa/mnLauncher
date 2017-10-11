a44f67d0e1db724d08896742b648957d75b51f26
Marc Nuri <marc@marcnuri.com>
2017-10-11 07:35:14 +0200

    Linux Icon Provider Added support for gnome theme icons

--- src/main/groovy/com/marcnuri/mnlauncher/icon/LinuxIconProvider.groovy
+++ src/main/groovy/com/marcnuri/mnlauncher/icon/LinuxIconProvider.groovy
@@ -12,19 +12,28 @@ import java.nio.file.Files
  */
 class LinuxIconProvider implements IconProvider {
 
-	private static final String XDG_DATA_DIRS = System.getenv("XDG_DATA_DIRS")
-	private static final String XDG_DATA_DIRS_ICON_SUFFIX = "/icons"
-	private static final String XDG_DATA_DIRS_HICOLOR_SUFFIX = "/hicolor/48x48/apps"
-	private static final String PIXMAPS = "/usr/share/pixmaps"
-	private static final String SH_SCRIPT_EXTENSION = ".sh"
+	private static final def PROCESS_TIMEOUT = 3000L
+
+	private static final def XDG_DATA_DIRS = System.getenv("XDG_DATA_DIRS")
+	private static final def XDG_DATA_DIRS_ICON_SUFFIX = "/icons"
+	private static final def DEFAULT_THEME_ICONS_DIRECTOERY = "48x48/apps"
+	private static final def XDG_DATA_DIRS_HICOLOR_SUFFIX = "/hicolor/$DEFAULT_THEME_ICONS_DIRECTOERY"
+	private static final def PIXMAPS = "/usr/share/pixmaps"
+	private static final def SH_SCRIPT_EXTENSION = ".sh"
+	private static final def GSETTINGS_COMMAND = "/usr/bin/gsettings"
+	private static final def GSETTINGS_COMMAND_ARGS = "get org.gnome.desktop.interface icon-theme"
 
 	private final boolean isLinux
+	private final Map<String, File> gnomeThemeIcons
 	private final Map<String, File> hicolorIcons
 	private final Map<String, File> pixmapsIcons
+
+	private String gnomeTheme
 
 
 	LinuxIconProvider() {
 		isLinux = System.getProperty(OS_PROPERTY_NAME).toLowerCase().contains("linux")
+		gnomeThemeIcons = new HashMap<>()
 		hicolorIcons = new HashMap<>()
 		pixmapsIcons = new HashMap<>()
 		initIndexes()
@@ -35,7 +44,9 @@ class LinuxIconProvider implements IconProvider {
 		Image ret = null
 		File command = new File(menuEntry.getFirstCommand())
 		if (command.exists() && command.isFile()) {
-			for(Map<String, File> fileMap : Arrays.asList(hicolorIcons, pixmapsIcons)) {
+			for(Map<String, File> fileMap : Arrays.asList(
+					// Search in order according to specification
+					gnomeThemeIcons, hicolorIcons, pixmapsIcons)) {
 				final String cName = command.getName()
 				ret = locateIcon(fileMap, cName)
 				// Locate sh script icon by name
@@ -55,11 +66,17 @@ class LinuxIconProvider implements IconProvider {
 	}
 
 	private initIndexes() {
+		//Detect Gnome Theme (if applies)
+		loadGnomeTheme()
 		// Load XDG Icons
 		if (XDG_DATA_DIRS != null) {
-			for(String xdgDir : XDG_DATA_DIRS.split(":")) {
+			for(def xdgDir : XDG_DATA_DIRS.split(":")) {
 				File xdgDirFile = new File(xdgDir.concat(XDG_DATA_DIRS_ICON_SUFFIX))
 				if (xdgDirFile.exists() && xdgDirFile.isDirectory()) {
+					// Try to add Gnome theme icons
+					addIconsToMap(gnomeThemeIcons,
+							new File(xdgDirFile, "$gnomeTheme/$DEFAULT_THEME_ICONS_DIRECTOERY"))
+					// Try to add Hicolor icons
 					addIconsToMap(hicolorIcons, new File(xdgDirFile, XDG_DATA_DIRS_HICOLOR_SUFFIX))
 				}
 			}
@@ -68,12 +85,25 @@ class LinuxIconProvider implements IconProvider {
 		addIconsToMap(pixmapsIcons, new File(PIXMAPS))
 	}
 
+	private loadGnomeTheme() {
+		if (new File(GSETTINGS_COMMAND).canExecute()) {
+			def out = new StringBuilder()
+			def err = new StringBuilder()
+			def process = [GSETTINGS_COMMAND, *GSETTINGS_COMMAND_ARGS.split(" ")].execute()
+			process.consumeProcessOutput(out, err)
+			process.waitForOrKill(PROCESS_TIMEOUT)
+			gnomeTheme = out.toString().
+					replace('\'', '').
+					replace('\n', '')
+		}
+	}
+
 	private static final Image locateIcon(Map<String, File> map, String name) {
-		Image ret = null;
+		Image ret = null
 		if (map.containsKey(name)) {
 			ret = ImageIO.read(map.get(name))
 		}
-		return ret;
+		return ret
 	}
 
 	private static final addIconsToMap(Map<String, File> map, File iconDir) {
